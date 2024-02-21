26a97cd24cab3bb074ec8c77ee04e15446f9e380
Index <57020946+IndexingGitHub@users.noreply.github.com>
2024-02-20 22:21:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -248,9 +248,11 @@ def event(name):
             parameters[i] = v
 
         if len(parameters) > 1:
-            parametersList = f"\n??? quote \"Parameters\"\n{'\n\n'.join(['\t' + item for item in parameters])}"
+            #parametersList = f"\n??? quote \"Parameters\"\n{'\n\n'.join(['\t' + item for item in parameters])}"
+            parametersList = f"\\n??? quote \\\"Parameters\\\"\\n{'\\n\\n'.join(['\\t' + item for item in parameters])}"
         elif len(parameters) == 1:
-            parametersList = f"\n!!! quote \"**Parameters:** <span style=\"font-weight: normal;\">" + parameters[0] + "</span>\""
+            #parametersList = f"\n!!! quote \"**Parameters:** <span style=\"font-weight: normal;\">" + parameters[0] + "</span>\""
+            parametersList = f"\\n!!! quote \\\"**Parameters:** <span style=\\\"font-weight: normal;\\\">{parameters[0]}</span>\\\""
 
     return "### :polytoria-Event: %s { #%s data-toc-label=\"%s\" }%s" % (name, name, name, parametersList)
 
